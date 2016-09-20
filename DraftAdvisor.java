a309218d149a662b217c1b6cda3946c4cdfba416
cancub <cancub@live.com>
2016-09-20 17:01:52 -0400

    detailing plan

--- DraftAdvisor.java
+++ DraftAdvisor.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.ArrayList;
 
 public class DraftAdvisor {
 	public static void main(String[] args) {
@@ -12,6 +13,8 @@ public class DraftAdvisor {
         Connection con = null;
         Statement pst = null;
         ResultSet rs = null;
+        
+        ArrayList<String> attributes = new ArrayList<String>();
 
         String url = "jdbc:mysql://localhost:3306/drafting";
         String user = "root";
@@ -22,16 +25,41 @@ public class DraftAdvisor {
             con = DriverManager.getConnection(url, user, password);
             pst = con.createStatement();
 //            rs = pst.executeQuery("SELECT MAX(G) as G FROM skaters");
-            rs = pst.executeQuery("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'goalies'");
+            rs = pst.executeQuery("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'goalies' OR TABLE_NAME = 'skaters'");
 
             while (rs.next()) {
                 
-                System.out.println(rs.getString(1));
+                attributes.add(rs.getString(1));
 //                System.out
             }
+            
+            // ask the user which attributes they want to use
+            
+            // ask the user for number of players on a team, number of goalies
+            
+            // locate the top values for each of these attributes
+            
+            // create a team comprised of each of these max attributes
+            
+            // ask the user for which players are already on their team
+            
+            // ask the user for which players are already on other teams
+            
+            // start suggesting players
+            
+            // ask the user to make a selection
+            
+            // ask the user if it is their turn yet
+            
+            // if it isn't, ask which player has been selected
+            
+            // if it is suggest players
+            
+            // repeat
 
         } catch (SQLException ex) {
-        
+
+            //myOrder.addSugar(2);    
             Logger lgr = Logger.getLogger(DraftAdvisor.class.getName());
             lgr.log(Level.SEVERE, ex.getMessage(), ex);
 
