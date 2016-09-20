a4247e50d69869f8c0610a64a732905ab10b74a2
cancub <cancub@live.com>
2016-09-20 16:45:52 -0400

    setting up with proper libraries

--- DraftAdvisor.java
+++ DraftAdvisor.java
@@ -1,6 +1,61 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DraftAdvisor {
 	public static void main(String[] args) {
 
-		System.out.println("test");
-	}
+        Connection con = null;
+        Statement pst = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:mysql://localhost:3306/drafting";
+        String user = "root";
+        String password = "supersecret";
+
+        try {
+            
+            con = DriverManager.getConnection(url, user, password);
+            pst = con.createStatement();
+//            rs = pst.executeQuery("SELECT MAX(G) as G FROM skaters");
+            rs = pst.executeQuery("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'goalies'");
+
+            while (rs.next()) {
+                
+                System.out.println(rs.getString(1));
+//                System.out
+            }
+
+        } catch (SQLException ex) {
+        
+            Logger lgr = Logger.getLogger(DraftAdvisor.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+            
+            try {
+                
+                if (rs != null) {
+                    rs.close();
+                }
+                
+                if (pst != null) {
+                    pst.close();
+                }
+                
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                
+                Logger lgr = Logger.getLogger(DraftAdvisor.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+    }
 }
